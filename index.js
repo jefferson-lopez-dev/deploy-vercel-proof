4d029b1db23d13997db97895e7a650d1fc07899a
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-20 16:24:42 -0500

    message

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3100;
 
-app.get("/hola", (req, res) => {
-  res.send("Hello, World!");
+app.get("/", (req, res) => {
+  res.send("Jefferson : What a temptation not to write 😥");
 });
 
 app.listen(port, () => {
